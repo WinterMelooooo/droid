779a7a05b964342d735349ba7711290c2f8726c5
WinterMelooooo <2538358797@qq.com>
2025-07-06 20:55:32 -0700

    [Laptop] Initial commit

--- scripts/server/launch_server.sh
+++ scripts/server/launch_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate polymetis-local
+source ~/miniforge3/etc/profile.d/conda.sh
+conda activate /home/ghr/miniforge3/envs/polymetis-local
 cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 python run_server.py
